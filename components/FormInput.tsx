028c822d7bb131d4f9f9f21dd5738162fe77d370
Hiep Doan <hiepdoan.cmcglobal@gmail.com>
2022-01-14 12:48:20 +0700

    assignment completed

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,21 +1,33 @@
+import { useEffect, useState } from "react";
+
 type Props = {
-  id: string,
-  label: string,
-  onInput: (input: string) => void,
-  errorMessage?: string,
-}
+  id: string;
+  label: string;
+};
 
 function wait(milliseconds: number) {
   const start = new Date().getTime();
   while (new Date().getTime() - start < milliseconds) {}
 }
-export default function FormInput({id, label, errorMessage, onInput}: Props) {
+export default function FormInput({ id, label }: Props) {
   // This component is a bit slow to render...
   wait(25);
 
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const onInput = (input: string) => {
+    if (input.length > 0 && input.length < 5) {
+      setErrorMessage("Minimum input length of 5 characters");
+    } else {
+      setErrorMessage(undefined);
+    }
+  };
+
   return (
     <div className="flex items-center">
-      <label htmlFor={id} className="flex-1">{label}</label>
+      <label htmlFor={id} className="flex-1">
+        {label}
+      </label>
       <div className="flex-1">
         <input
           type="text"
